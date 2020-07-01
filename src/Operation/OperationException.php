2c28401388f8200d94fb8c08a8c9614dcebbd508
Grenier jérémie <jgrenier@coffreo.com>
2020-07-01 17:17:03 +0200

    feat(operation): add all classes to manage operations

---
+++ src/Operation/OperationException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of grade-api project
+ */
+
+namespace App\Operation;
+
+/**
+ * Class OperationException.
+ *
+ * @author jgrenier
+ *
+ * @version 1.0.0
+ */
+class OperationException extends \Exception
+{
+}
